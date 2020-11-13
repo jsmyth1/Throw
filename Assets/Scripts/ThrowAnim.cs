56a0621f6517676ea1b9c6c1fe2ddf5030f508b5
James Smyth <162642@escg.ac.uk>
2020-11-13 14:48:10 +0000

    Initial commit

---
+++ Assets/Scripts/ThrowAnim.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowAnim : MonoBehaviour
+{
+    public Animator Ani;
+
+    bool Throw = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Throw = true;
+            Ani.SetBool("throw", true);
+        }
+
+        else if (Input.GetKeyUp(KeyCode.T))
+        {
+            Throw = false;
+            Ani.SetBool("throw", false);
+        }
+    }
+}
