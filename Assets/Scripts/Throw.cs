8d9e85406bdaa6942a15d46d7163f28d8a81569c
James Smyth <162642@escg.ac.uk>
2020-11-17 20:17:56 +0000

    added new script

---
+++ Assets/Scripts/Throw.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throw : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Rigidbody>().useGravity = false;
+    }
+
+    public void Hold()
+    {
+        GetComponent<Rigidbody>().useGravity = false;
+        GetComponent<SphereCollider>().enabled = false;
+    }
+    public void ThrowBall()
+    {
+        GetComponent<Rigidbody>().AddForce(transform.forward * 500);
+        this.transform.parent = null;
+        GetComponent<Rigidbody>().useGravity = true;
+
+    }
+}
+
+
+
