f27504189c9b091e7d838726b90585507046c5ed
James Smyth <162642@escg.ac.uk>
2020-11-18 07:45:44 +0000

    added new script

---
+++ Assets/Scripts/Animations.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animations : MonoBehaviour
+{
+    public Animator anim;
+
+    public Throw throwScript;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            anim.Play("throw");
+        }
+    }
+
+    void ThrowTheBall()
+    {
+        throwScript.ThrowBall();
+    }
+}
+
